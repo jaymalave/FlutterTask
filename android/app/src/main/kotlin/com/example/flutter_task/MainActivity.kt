e61092c03b61d494d244c011714716b3fcb19d6c
jaymalave <jaym.github@gmail.com>
2021-12-14 23:31:51 +0530

    link firebase, basic setup

---
+++ android/app/src/main/kotlin/com/example/flutter_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jaymalave.fluttertask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,21 @@
+import 'dart:html';
+
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final int phone;
+  final String category;
+  final String dp;
+  final String bio;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.phone,
+    required this.category,
+    required this.dp,
+    required this.bio,
+  });
+}
